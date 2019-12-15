7888056cb881623ffca331edc70fbeab66181cda
Michel Kremer <kremi151dev@gmail.com>
2019-12-15 22:33:27 +0100

    Improve package.json generation in CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { dest, series, src, task } = require('gulp');
 const ts = require('gulp-typescript');
 const fs = require('fs');
+const log = require('fancy-log');
 
 task('build', function () {
     const tsProject = ts.createProject('tsconfig.json');
@@ -12,10 +13,20 @@ task('build', function () {
 task('package_json', function(cb) {
     const packageJsonContent = fs.readFileSync('package.json', { encoding: 'utf8' });
     let packageJson = JSON.parse(packageJsonContent);
+
     delete packageJson.scripts;
     delete packageJson.devDependencies;
+    delete packageJson.resolutions;
+
     packageJson.main = 'lib/index.js';
     packageJson.typings = 'lib/index.d.ts';
+
+    const ciVersion = process.env.CI_BUILD_VERSION;
+    if (ciVersion) {
+        log(`Using custom version number ${ciVersion}`);
+        packageJson.version = ciVersion;
+    }
+
     fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, null, 2), { encoding: 'utf8' });
     cb();
 });
