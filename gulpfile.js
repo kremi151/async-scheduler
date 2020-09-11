8ce052ed917df00f8a166e76b1dbfd43d391b8ef
Michel Kremer <kremi151dev@gmail.com>
2020-09-12 00:19:29 +0200

    Migrate core logic of Jenkinsfile to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,42 @@
 const { dest, series, src, task } = require('gulp');
 const ts = require('gulp-typescript');
 const fs = require('fs');
+const path = require('path');
 const log = require('fancy-log');
 
-task('build', function () {
+let asVersion = null;
+
+task('determine_version', (cb) => {
+    const { AS_VERSION } = process.env;
+    if (!AS_VERSION) {
+        log('No version set via environment variables, this is not a release build');
+        cb();
+        return;
+    }
+    const match = /([0-9]+)\.([0-9]+)\.([0-9]+)/.exec(AS_VERSION);
+    if (!match) {
+        log.error(`Given value for environment variable AS_VERSION does not contain a valid version string: ${AS_VERSION}`);
+        cb('Invalid version string');
+        return;
+    }
+    asVersion = {
+        major: Number.parseInt(match[1]),
+        minor: Number.parseInt(match[2]),
+        patch: Number.parseInt(match[3]),
+    };
+    log(`Detected version ${asVersion.major}.${asVersion.minor}.${asVersion.patch}`);
+    cb();
+});
+
+task('compile', function () {
     const tsProject = ts.createProject('tsconfig.json');
     return src('src/**/*.ts')
         .pipe(tsProject())
         .pipe(dest('dist/lib'));
 });
 
-task('package_json', function(cb) {
-    const packageJsonContent = fs.readFileSync('package.json', { encoding: 'utf8' });
-    let packageJson = JSON.parse(packageJsonContent);
+task('package_json', async () => {
+    const packageJson = await fs.promises.readFile('package.json', { encoding: 'utf8' }).then(JSON.parse);
 
     delete packageJson.scripts;
     delete packageJson.devDependencies;
@@ -20,15 +44,19 @@ task('package_json', function(cb) {
 
     packageJson.main = 'lib/index.js';
     packageJson.typings = 'lib/index.d.ts';
+    packageJson.files = [
+        'lib/**',
+        'LICENSE',
+        'README',
+    ];
 
-    const ciVersion = process.env.CI_BUILD_VERSION;
-    if (ciVersion) {
-        log(`Using custom version number ${ciVersion}`);
-        packageJson.version = ciVersion;
+    if (asVersion) {
+        const version = `${asVersion.major}.${asVersion.minor}.${asVersion.patch}`;
+        log(`Using release version number ${version}`);
+        packageJson.version = version;
     }
 
-    fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, null, 2), { encoding: 'utf8' });
-    cb();
+    await fs.promises.writeFile(path.join('dist', 'package.json'), JSON.stringify(packageJson, null, 2), { encoding: 'utf8' });
 });
 
 task('readme', function(cb) {
@@ -37,4 +65,37 @@ task('readme', function(cb) {
     cb();
 });
 
-task('default', series(['build', 'package_json', 'readme']));
+async function packageJsonContainsVersion(path, expectedVersion) {
+    const pjson = await fs.promises.readFile(path, { encoding: 'utf8' }).then(JSON.parse);
+    const pjsonVersion = `${pjson.version}`;
+    if (pjsonVersion !== expectedVersion) {
+        throw new Error(`${path} contains wrong version "${pjsonVersion}", expected "${expectedVersion}"`);
+    }
+}
+
+task('check_package_json_version', async () => {
+    if (!asVersion) {
+        log('Not a release build, skipping check');
+        return;
+    }
+    const expectedVersion = `${asVersion.major}.${asVersion.minor}.${asVersion.patch}`;
+    await packageJsonContainsVersion(path.resolve('package.json'), expectedVersion);
+    await packageJsonContainsVersion(path.resolve('dist', 'package.json'), expectedVersion);
+});
+
+task('build', series([
+    'determine_version',
+    'compile',
+    'package_json',
+    'readme',
+]));
+
+task('validate', series([
+    'determine_version',
+    'check_package_json_version',
+]));
+
+task('default', series([
+    'build',
+    'validate',
+]));
